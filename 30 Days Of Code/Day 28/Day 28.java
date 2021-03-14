b38631366366ef8cf2df1fe7e9ca2ab60f808996
Zdenko Filip <67583742+Z-dip@users.noreply.github.com>
2021-03-14 15:25:52 +0100

    Create Day 28.java

---
+++ 30 Days Of Code/Day 28/Day 28.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        String Regrex=".+@gmail\\.com$";
+        List<String> list= new ArrayList();
+        Pattern pattern=Pattern.compile(Regrex);
+
+        for (int NItr = 0; NItr < N; NItr++) {
+            String[] firstNameEmailID = scanner.nextLine().split(" ");
+
+            String firstName = firstNameEmailID[0];
+
+            String emailID = firstNameEmailID[1];
+            
+            Matcher matcher=pattern.matcher(emailID);
+            
+            if(matcher.find()){
+                list.add(firstName);
+            }
+        }
+        Collections.sort(list);
+        
+        for(String names:list){
+            System.out.println(names);
+        }
+        scanner.close();
+    }
+}
