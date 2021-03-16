54f0c26833f582ebc98f1dade02f3bfd12601600
Zdenko Filip <67583742+Z-dip@users.noreply.github.com>
2021-03-16 18:57:30 +0100

    Create Java BigInteger.java

---
+++ Java Road/Java BigInteger/Java BigInteger.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        BigInteger bi1 = new BigInteger(scanner.next());
+        BigInteger bi2 = new BigInteger(scanner.next());
+        System.out.println(bi1.add(bi2) +"\n" + bi1.multiply(bi2));
+      
+     
+
+    }
+}
