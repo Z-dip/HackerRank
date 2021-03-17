e24ee02b993d5d8785db778b838e67b7bcd5518e
Zdenko Filip <67583742+Z-dip@users.noreply.github.com>
2021-03-17 15:34:52 +0100

    Create Java 2D Array.java

---
+++ Java Road/Java 2D Array/Java 2D Array.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        int maxSum = Integer.MIN_VALUE;;
+        int sum = 0;
+            
+        for(int i=0; i < 6; i++)
+        {
+            for(int j=0; j < 6; j++)
+            {
+                arr[i][j] = in.nextInt();
+                
+                if(i > 1 && j > 1)
+                {
+                    sum = arr[i][j] + arr[i][j - 1] + arr[i][j - 2] 
+                        + arr[i - 1][j - 1] 
+                        + arr[i - 2][j] + arr[i - 2][j - 1] + arr[i -2][j - 2];
+                    
+                    if(sum > maxSum)
+                    {
+                        maxSum = sum;
+                    }
+                }
+            }
+        }
+        
+        System.out.println(maxSum);
+    }
+}
