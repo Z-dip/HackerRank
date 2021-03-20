ccb551527fc64a9424ca3c54bcc493dad7015ed7
Zdenko Filip <67583742+Z-dip@users.noreply.github.com>
2021-03-20 19:17:50 +0100

    Create Java Iterator.java

---
+++ Java Road(/Java Iterator/Java Iterator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+	
+   static Iterator func(ArrayList mylist){
+      Iterator it=mylist.iterator();
+      while(it.hasNext()){
+         Object element = it.next();
+         if(element instanceof String)//Hints: use instanceof operator
+
+			break;
